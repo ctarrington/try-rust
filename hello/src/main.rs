ccdc3b355a61bdfb781e0a1de2a035b439bec4a3
ctarrington <arrington.ct@gmail.com>
2021-07-09 12:56:02 -0400

    tinkering w tests

---
+++ hello/src/main.rs
@@ -0,0 +1,31 @@
+use std::mem;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+  assert!(n != 0 && m != 0);
+
+  while m != 0 {
+    if m < n {
+      mem::swap(&mut n, &mut m);
+    }
+
+    m = m % n;
+  }
+
+  n
+}
+
+#[test]
+fn test_gcd() {
+  assert_eq!(gcd(14, 15), 1);
+  assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11);
+}
+
+#[test]
+#[should_panic]
+fn test_zeros() {
+  gcd(0, 15);
+}

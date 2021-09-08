0a6ad8a8772cdd46df94986fed150a7255049eba
ctarrington <arrington.ct@gmail.com>
2021-09-08 19:00:31 -0400

    comments and keep the extrema in the moving average

--- wasm-comparisons/life-comparison/www/index.js
+++ wasm-comparisons/life-comparison/www/index.js
@@ -4,6 +4,8 @@ import { createMovingAverage } from './utils';
 const pre = document.getElementById('game-of-life-text');
 const elapsedSpan = document.getElementById('elapsed-span');
 const averageElapsedSpan = document.getElementById('average-elapsed-span');
+const minimumElapsedSpan = document.getElementById('minimum-elapsed-span');
+const maximumElapsedSpan = document.getElementById('maximum-elapsed-span');
 
 const universe = Universe.new();
 const movingAverage = createMovingAverage(1000);
@@ -15,9 +17,13 @@ const renderLoop = () => {
 
   const elapsed = performance.now() - start;
   const average = movingAverage.add(elapsed);
+  const minimum = movingAverage.min();
+  const maximum = movingAverage.max();
 
   elapsedSpan.textContent = elapsed.toFixed(2);
   averageElapsedSpan.textContent = average.toFixed(2);
+  minimumElapsedSpan.textContent = minimum.toFixed(2);
+  maximumElapsedSpan.textContent = maximum.toFixed(2);
 
   requestAnimationFrame(renderLoop);
 };
--- wasm-comparisons/life-comparison/www/utils.js
+++ wasm-comparisons/life-comparison/www/utils.js
@@ -1,8 +1,11 @@
 
+// use a circular buffer of the passed size to calculate a moving or windowed average
 const createMovingAverage = (thesize) => {
   const size = thesize;
   let currentPosition = 0;
   let sum = 0;
+  let minValue = Number.MAX_VALUE;
+  let maxValue = Number.MIN_VALUE;
   const values = [];
 
   const add = (value) => {
@@ -13,13 +16,21 @@ const createMovingAverage = (thesize) => {
     }
     values[currentPosition] = value;
     sum += value;
+
+    minValue = Math.min(minValue, value);
+    maxValue = Math.max(maxValue, value);
+
+    // increment and circle around if at the end 
     currentPosition++;
     currentPosition = currentPosition % size;
 
     return sum/values.length;
   };
+
+  const min = () => { return minValue; };
+  const max = () => { return maxValue; };
   
-  return {add};
+  return {add, min, max};
 };
 
 export {createMovingAverage};

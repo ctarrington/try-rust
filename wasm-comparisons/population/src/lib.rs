aae81d3b4e6e45bbab10ff5ecdfc8135f1eb1501
ctarrington <arrington.ct@gmail.com>
2021-10-20 21:30:33 -0400

    ugly comparison of lcg

--- wasm-comparisons/population/src/lib.rs
+++ wasm-comparisons/population/src/lib.rs
@@ -15,15 +15,44 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, population!");
-}
-
-#[wasm_bindgen]
-pub fn mean(count: u16) -> f32 {
+pub fn random_mean(count: u16) -> f32 {
     let mut sum: f32 = 0.0;
     for _ in 0..count {
         let value: f32 = random();
+        sum += value;
+    }
+    sum / f32::from(count)
+}
+
+struct LCG {
+    current: f32,
+    a: f32,
+    c: f32,
+    m: f32,
+}
+
+impl LCG {
+    fn next(&mut self) -> f32 {
+        self.current = (self.a * self.current + self.c) % self.m;
+        self.current / self.m
+    }
+}
+
+fn lcg(seed: f32) -> LCG {
+    LCG {
+        current: seed,
+        a: 8121.0,
+        c: 28411.0,
+        m: 134456.0,
+    }
+}
+
+#[wasm_bindgen]
+pub fn lcg_mean(count: u16) -> f32 {
+    let mut the_lcg = lcg(123456789.0);
+    let mut sum: f32 = 0.0;
+    for _ in 0..count {
+        let value: f32 = the_lcg.next();
         sum += value;
     }
     sum / f32::from(count)
--- wasm-comparisons/population/www/index.js
+++ wasm-comparisons/population/www/index.js
@@ -1,6 +1,6 @@
 import * as wasm from "population";
 
-const mean = (count) => {
+const random_mean = (count) => {
   let sum = 0;
 
   for (let ctr = 0; ctr < count; ctr++) {
@@ -10,28 +10,71 @@ const mean = (count) => {
   return sum / count;
 };
 
-const rustMeanSpan = document.getElementById('rust-mean');
-const rustElapsedSpan = document.getElementById('rust-elapsed');
-const jsMeanSpan = document.getElementById('js-mean');
-const jsElapsedSpan = document.getElementById('js-elapsed');
+const random_lcg = (count) => {
+  let current = 123456789.0;
+  const a = 8121.0;
+  const c = 28411.0;
+  const m = 134456.0;
+  const lcg_next = () => {
+        current = (a * current + c) % m;
+        return current / m;
+  };
+
+  let sum = 0;
+
+  for (let ctr = 0; ctr < count; ctr++) {
+    sum += lcg_next();
+  }
+
+  return sum / count;
+}
+
+const rustRandomMeanSpan = document.getElementById('rust-random-mean');
+const rustRandomElapsedSpan = document.getElementById('rust-random-elapsed');
+
+const jsRandomMeanSpan = document.getElementById('js-random-mean');
+const jsRandomElapsedSpan = document.getElementById('js-random-elapsed');
+
+const rustLcgMeanSpan = document.getElementById('rust-lcg-mean');
+const rustLcgElapsedSpan = document.getElementById('rust-lcg-elapsed');
+
+const jsLcgMeanSpan = document.getElementById('js-lcg-mean');
+const jsLcgElapsedSpan = document.getElementById('js-lcg-elapsed');
+
 const countDiv = document.getElementById('count');
 const runButton = document.getElementById('run-button');
 
 let count = 1000*1000;
 
 const tick = () => {
-  const rustStart = performance.now();
-  const rustMean = wasm.mean(count);
-  const rustElapsed = performance.now() - rustStart;
+  const rustRandomStart = performance.now();
+  const rustRandomMean = wasm.random_mean(count);
+  const rustRandomElapsed = performance.now() - rustRandomStart;
 
-  const jsStart = performance.now();
-  const jsMean = mean(count);
-  const jsElapsed = performance.now() - jsStart;
+  const jsRandomStart = performance.now();
+  const jsRandomMean = random_mean(count);
+  const jsRandomElapsed = performance.now() - jsRandomStart;
 
-  rustMeanSpan.textContent = rustMean.toFixed(5);
-  rustElapsedSpan.textContent = rustElapsed.toFixed(2);
-  jsMeanSpan.textContent = jsMean.toFixed(5);
-  jsElapsedSpan.textContent = jsElapsed.toFixed(2);
+  const rustLcgStart = performance.now();
+  const rustLcgMean = wasm.lcg_mean(count);
+  const rustLcgElapsed = performance.now() - rustLcgStart;
+
+  const jsLcgStart = performance.now();
+  const jsLcgMean = random_lcg(count);
+  const jsLcgElapsed = performance.now() - jsLcgStart;
+
+  rustRandomMeanSpan.textContent = rustRandomMean.toFixed(5);
+  rustRandomElapsedSpan.textContent = rustRandomElapsed.toFixed(2);
+
+  jsRandomMeanSpan.textContent = jsRandomMean.toFixed(5);
+  jsRandomElapsedSpan.textContent = jsRandomElapsed.toFixed(2);
+
+  rustLcgMeanSpan.textContent = rustLcgMean.toFixed(5);
+  rustLcgElapsedSpan.textContent = rustLcgElapsed.toFixed(2);
+
+  jsLcgMeanSpan.textContent = jsLcgMean.toFixed(5);
+  jsLcgElapsedSpan.textContent = jsLcgElapsed.toFixed(2);
+
   countDiv.textContent = count.toLocaleString();
 
   count += 1000*1000;

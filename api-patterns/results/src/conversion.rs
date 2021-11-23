fecd795549f1410a87c53ea94e5bd97302c1cce5
ctarrington <arrington.ct@gmail.com>
2021-11-23 15:06:23 -0500

    meh starting point

--- api-patterns/results/src/conversion.rs
+++ api-patterns/results/src/conversion.rs
@@ -1,19 +1,55 @@
 // mess around with an external api that needs to be converted to internal errors
+// backend errors to front end responses
+
 #[derive(Debug)]
-enum Flavor {
+pub enum Flavor {
     Vanilla,
     Chocolate,
 }
 
 #[derive(Debug)]
-struct Thing {
-    size: i32,
+pub struct Thing {
+    size: u32,
     flavor: Option<Flavor>,
+}
+
+#[derive(Debug)]
+pub enum Payment {
+    Cash(u32),
+    Card(bool),
+}
+
+pub struct SimpleKitchen {}
+
+impl SimpleKitchen {
+    fn prepare(thing: Thing) -> Result<(), &'static str> {
+        Ok(())
+    }
+}
+
+pub struct Cashier<'a> {
+    kitchen: &'a SimpleKitchen,
+}
+
+impl<'a> Cashier<'a> {
+    pub fn new(kitchen: &'a SimpleKitchen) -> Self {
+        Self { kitchen: kitchen }
+    }
+
+    pub fn buy(&self, thing: Thing, payment: Payment) -> Result<u32, &'static str> {
+        match payment {
+            Payment::Card(true) => Ok(0),
+            Payment::Card(false) => Err("Card declined"),
+
+            Payment::Cash(value) if value >= thing.size => Ok(value - thing.size),
+            Payment::Cash(_) => Err("Not enough cash"),
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::conversion::{Flavor, Thing};
+    use crate::conversion::{Cashier, Flavor, Payment, SimpleKitchen, Thing};
 
     #[test]
     fn simple() {
@@ -24,5 +60,19 @@ mod tests {
 
         assert_eq!(thing.size, 22);
         assert!(matches!(thing.flavor, Some(Flavor::Chocolate)));
+    }
+
+    #[test]
+    fn happy_path() {
+        let kitchen = SimpleKitchen {};
+        let cashier: Cashier = Cashier::new(&kitchen);
+
+        let thing = Thing {
+            size: 22,
+            flavor: Some(Flavor::Vanilla),
+        };
+
+        let response = cashier.buy(thing, Payment::Cash(100u32));
+        assert!(matches!(response, Ok(78)));
     }
 }

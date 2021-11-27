ce51559d41da702750acee91d273ec8a724d7ed4
CT Arrington <arrington.ct@gmail.com>
2021-11-27 17:11:25 -0500

    cleanup and parse as object

--- api-patterns/parse-json/src/lib.rs
+++ api-patterns/parse-json/src/lib.rs
@@ -1,20 +1,33 @@
 #[cfg(test)]
 mod tests {
+    use serde::{Deserialize, Serialize};
     use serde_json::{Result, Value};
 
-    #[test]
-    fn parse_raw_contact() -> Result<()> {
-        let raw = r#"
-        {"fred": 
+    fn get_raw() -> &'static str {
+        r#"
             {"name": "Fred"}
-        }
-        "#;
+        "#
+    }
 
-        println!("raw: {}", raw);
-
+    #[test]
+    fn parse_as_value() -> Result<()> {
+        let raw = get_raw();
         let parsed_value: Value = serde_json::from_str(raw)?;
-        assert_eq!(parsed_value["fred"]["name"], "Fred");
-        assert_eq!(parsed_value["fred"]["address"]["street1"], Value::Null);
+        assert_eq!(parsed_value["name"], "Fred");
+        assert_eq!(parsed_value["address"]["street1"], Value::Null);
+        Ok(())
+    }
+
+    #[test]
+    fn parse_as_object() -> Result<()> {
+        #[derive(Deserialize, Serialize)]
+        struct Contact {
+            name: String,
+        }
+
+        let raw = get_raw();
+        let fred: Contact = serde_json::from_str(raw)?;
+        assert_eq!(fred.name, "Fred");
         Ok(())
     }
 }

6d2b7fb098222584b24c374cdeba5a9af20fbfff
CT Arrington <arrington.ct@gmail.com>
2021-11-29 21:42:01 -0500

    rand

--- api-patterns/parse-json/src/lib.rs
+++ api-patterns/parse-json/src/lib.rs
@@ -1,26 +1,59 @@
+use rand::distributions::uniform::Uniform;
+use rand::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Address {
+    street1: String,
+    street2: String,
+    city: String,
+    state: String,
+    zip: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Contact {
+    id: String,
+    name: String,
+    address: Address,
+}
+
+// todo: wrap up in an iterator
+fn create_random_contact(id: &str) -> Contact {
+    let names: Vec<String> = ["Ted", "Fred", "Barney", "Betty", "Wilma"]
+        .iter()
+        .map(|&s| s.into())
+        .collect();
+
+    let mut rng = rand::thread_rng();
+    let uniform_indexes = Uniform::from(0..names.len());
+    let index = uniform_indexes.sample(&mut rng);
+    let name = names.get(index).unwrap().to_string();
+
+    let address = Address {
+        street1: "123 Main Street".to_string(),
+        street2: "".to_string(),
+        city: "nowhere".to_string(),
+        state: "md".to_string(),
+        zip: "21228".to_string(),
+    };
+
+    Contact {
+        id: id.to_string(),
+        name,
+        address,
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use serde::{Deserialize, Serialize};
+    use crate::{create_random_contact, Contact};
     use serde_json::{Result, Value};
-
-    #[derive(Deserialize, Serialize)]
-    struct Address {
-        street1: String,
-        street2: String,
-        city: String,
-        state: String,
-        zip: String,
-    }
-
-    #[derive(Deserialize, Serialize)]
-    struct Contact {
-        name: String,
-        address: Address,
-    }
 
     fn get_raw() -> &'static str {
         r#"
-            {"name": "Fred",
+            {"id": "123",
+            "name": "Fred",
             "address": {
                 "street1": "123 Main Street",
                 "street2": "",
@@ -49,5 +82,12 @@ mod tests {
         assert_eq!(fred.name, "Fred");
         assert_eq!(fred.address.city, "Smalltown");
         Ok(())
+    }
+
+    #[test]
+    fn create_contact_json() {
+        let contact = create_random_contact("123");
+        println!("contact {:?}", contact);
+        assert_eq!(contact.id, "123");
     }
 }

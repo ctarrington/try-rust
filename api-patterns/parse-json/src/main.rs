3a290c3398792a8f72fb8a4922b54c45531da2bd
CT Arrington <arrington.ct@gmail.com>
2021-12-01 22:22:30 -0500

    iterate over the contacts and write them to std out

--- api-patterns/parse-json/src/main.rs
+++ api-patterns/parse-json/src/main.rs
@@ -2,7 +2,9 @@ use parse_json::RandomContactIterator;
 
 fn main() {
     println!("hello from parse-json");
-    let mut contact_iterator = RandomContactIterator::new(0, 2);
-    let contact = contact_iterator.next();
-    println!("contact {:?}", contact);
+    let mut contact_iterator = RandomContactIterator::new(0, 3);
+
+    while let Some(contact) = contact_iterator.next() {
+        println!("contact {:?}", serde_json::to_string(&contact));
+    }
 }

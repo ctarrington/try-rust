283fabb82047954758626bc495fdd5dc6b20d58d
CT Arrington <arrington.ct@gmail.com>
2021-12-02 21:14:25 -0500

    serialize and deserialize

--- api-patterns/parse-json/src/main.rs
+++ api-patterns/parse-json/src/main.rs
@@ -1,10 +1,14 @@
-use parse_json::RandomContactIterator;
+use parse_json::{Contact, RandomContactIterator};
 
-fn main() {
-    println!("hello from parse-json");
+fn main() -> Result<(), std::io::Error> {
     let mut contact_iterator = RandomContactIterator::new(0, 3);
 
     while let Some(contact) = contact_iterator.next() {
-        println!("contact {:?}", serde_json::to_string(&contact));
+        let serialized_contact = serde_json::to_string(&contact)?;
+        println!("contact {:?}", serialized_contact);
+        let deserialized_contact: Contact = serde_json::from_str(&serialized_contact)?;
+        println!("deserialized_contact {:?}", deserialized_contact);
     }
+
+    Ok(())
 }

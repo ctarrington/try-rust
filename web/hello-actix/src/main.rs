d454c38f3b6d4c360563313315d78703f494b623
CT Arrington <arrington.ct@gmail.com>
2021-12-20 22:03:26 -0500

    primitive shared and updated state

--- web/hello-actix/src/main.rs
+++ web/hello-actix/src/main.rs
@@ -1,13 +1,43 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::web::Data;
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use std::sync::Mutex;
+use std::thread;
+use std::time::Duration;
+
+struct SharedState {
+    count: Mutex<u32>,
+}
 
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world! From actix-web. How are you?")
+async fn hello(data: web::Data<SharedState>) -> impl Responder {
+    let count = &data.count;
+    HttpResponse::Ok().body(format!(
+        "Hello world! From actix-web. How are you? count: {}",
+        *count.lock().unwrap()
+    ))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello))
+    let shared_state = Data::new(SharedState {
+        count: Mutex::new(0),
+    });
+
+    // copy of pointer for use in the thread
+    let shared_state_for_thread = shared_state.clone();
+    let handle = thread::spawn(move || loop {
+        println!("about to increment");
+        {
+            let mut count = shared_state_for_thread.count.lock().unwrap();
+            *count += 1;
+        }
+        println!("back from increment, about to sleep");
+        thread::sleep(Duration::from_secs(1));
+        println!("back from sleep");
+    });
+
+    // move a copy of the reference counted pointer to the shared state
+    HttpServer::new(move || App::new().app_data(shared_state.clone()).service(hello))
         .bind("127.0.0.1:8080")?
         .run()
         .await

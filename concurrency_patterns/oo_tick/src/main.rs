deb1330d39231ceaa374c8791b0e34bf5c1b0790
CT Arrington <arrington.ct@gmail.com>
2021-12-25 09:48:14 -0500

    cleanup

--- concurrency_patterns/oo_tick/src/main.rs
+++ concurrency_patterns/oo_tick/src/main.rs
@@ -16,44 +16,41 @@ impl Thing {
     }
 }
 
-/// make slices for the elements, the slices may be slightly different sizes and for small
-/// elements sizes the last slice may not be created
-fn calculate_execution_slices<T>(
-    elements: &Vec<Arc<Mutex<T>>>,
-    thread_count: usize,
-) -> Vec<(usize, usize)> {
-    let mut execution_slices = Vec::new();
+/// make blocks for the elements, they may be slightly different sizes and for small
+/// sizes the last block may not be created
+fn calculate_execution_blocks(size: usize, thread_count: usize) -> Vec<(usize, usize)> {
+    let mut execution_blocks = Vec::new();
 
-    let stop = elements.len() - 1;
-    let raw_stride = (elements.len() as f32) / thread_count as f32;
-    let stride = raw_stride.ceil() as usize + 1;
+    let stop = size - 1;
+    let raw_stride = (size as f32) / thread_count as f32;
+    let stride = raw_stride.ceil() as usize;
 
     let mut start_index = 0;
     for _ in 0..thread_count {
-        let stop_index = start_index + stride - 1;
+        let stop_index = start_index + stride;
         let stop_index = cmp::min(stop_index, stop);
         if stop_index >= start_index {
-            execution_slices.push((start_index, stop_index));
+            execution_blocks.push((start_index, stop_index));
         }
-        start_index += stride;
+        start_index = start_index + stride + 1;
     }
-    execution_slices
+    execution_blocks
 }
 
 fn main() {
     let things: Vec<Arc<Mutex<Thing>>> = (0..10)
         .map(|_| Arc::new(Mutex::new(Thing::new())))
         .collect();
-    let blocks = calculate_execution_slices(&things, 3);
+
+    println!("before things {:?}", things);
+    let blocks = calculate_execution_blocks(things.len(), 3);
     println!("blocks: {:?} ", blocks);
 
     let mut handles = Vec::new();
     let things = Arc::new(things);
 
     blocks.iter().for_each(|block| {
-        let (start_index, stop_index) = block;
-        let start_index = *start_index;
-        let stop_index = *stop_index;
+        let (start_index, stop_index) = (block.0, block.1);
         let things_for_threads = things.clone();
 
         handles.push(thread::spawn(move || {
@@ -68,5 +65,5 @@ fn main() {
         handle.join().expect("unable to join tick handle");
     }
 
-    println!("after things {:?}", things);
+    println!("\nafter things {:?}", things);
 }

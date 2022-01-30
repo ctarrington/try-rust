be38792979b5fae384d32c10fdc122d3cdd792da
CT Arrington <arrington.ct@gmail.com>
2022-01-30 14:08:47 -0500

    moving a svg

--- dioxus/hello-web/src/main.rs
+++ dioxus/hello-web/src/main.rs
@@ -8,6 +8,7 @@ fn main() {
 #[allow(non_snake_case)]
 fn App(cx: Scope) -> Element {
     let (name, set_name) = use_state(&cx, || "Jane");
+    let (red_x_position, set_red_x_position) = use_state(&cx, || 0);
 
     let circles = (0..=10).map(|value| {
         let x_position = value*10;
@@ -15,20 +16,34 @@ fn App(cx: Scope) -> Element {
 
        rsx! {
             circle {
+                key: "{value}",
+                fill: "black",
                 cx: "{x_position}",
                 cy: "{y_position}",
                 r: "5",
             }
        }
     });
+
+    let red_circle = rsx! {
+        circle {
+            fill: "red",
+            cx: "{red_x_position}",
+            cy: "40",
+            r: "3",
+        }
+    };
+
     cx.render(rsx! {
         div {
             div { "Hello to {name}" }
             button { onclick: move |_| set_name("Fred"), "Fred"}
             button { onclick: move |_| set_name("Ted"), "Ted"}
+            button { onclick: move |_| set_red_x_position(red_x_position + 1), "X+"}
             svg {
                 view_box: "0 0  100 100",
                 circles
+                red_circle
             }
             }
     })

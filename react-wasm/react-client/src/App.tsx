4de83743279f6cc552c37880d31b44aec76d5348
CT Arrington <arrington.ct@gmail.com>
2022-02-01 21:10:28 -0500

    load wasm async in index before render

--- react-wasm/react-client/src/App.tsx
+++ react-wasm/react-client/src/App.tsx
@@ -2,31 +2,44 @@ import React, { useEffect, useState } from "react";
 import { useInterval } from "usehooks-ts";
 import "./App.css";
 
-function App() {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Person } from "wasm";
+
+type AppProps = {
+  wasm: any;
+};
+
+function App({ wasm }: AppProps) {
+  let {
+    Person,
+    add_two_ints,
+    get_person,
+    get_people,
+    get_numbers,
+    greet,
+    format_name,
+  } = wasm;
+
   useEffect(() => {
-    import("wasm").then(
-      ({
-        Person,
-        add_two_ints,
-        get_person,
-        get_people,
-        get_numbers,
-        greet,
-        format_name,
-      }) => {
-        setNumbers(get_numbers(5));
-        setSum(add_two_ints(10, 20));
-        setGreeting(greet("Rusty"));
-        setName(format_name(Person.new("Fred", "Fredrickson")));
-        const joe = get_person();
-        joe.tick();
-        setPersonName(joe.first_name());
-        setPersonCoins(joe.coins());
-        setPerson(joe);
-        setPeople(get_people(2));
-      }
-    );
-  }, []);
+    setNumbers(get_numbers(5));
+    setSum(add_two_ints(10, 20));
+    setGreeting(greet("Rusty"));
+    setName(format_name(Person.new("Fred", "Fredrickson")));
+    const joe = get_person();
+    joe.tick();
+    setPersonName(joe.first_name());
+    setPersonCoins(joe.coins());
+    setPerson(joe);
+    setPeople(get_people(2));
+  }, [
+    Person,
+    add_two_ints,
+    get_person,
+    get_people,
+    get_numbers,
+    greet,
+    format_name,
+  ]);
 
   const [numbers, setNumbers] = useState<Uint32Array>(Uint32Array.from([]));
   const [sum, setSum] = useState<number>(0);
@@ -34,8 +47,8 @@ function App() {
   const [name, setName] = useState<string>("");
   const [personName, setPersonName] = useState<string>("");
   const [personCoins, setPersonCoins] = useState<number>(0);
-  const [person, setPerson] = useState<any>(null);
-  const [people, setPeople] = useState<any>(null);
+  const [person, setPerson] = useState<Person | null>(null);
+  const [people, setPeople] = useState<[Person] | null>(null);
 
   useInterval(() => {
     if (person) {
@@ -54,7 +67,7 @@ function App() {
       <div>Numbers: {numbers.join(",")}</div>
       {people && (
         <div>
-          People:{" "}
+          People:
           {people.map((person: any, index: number) => (
             <span key={index}>
               {person.first_name} {person.last_name} {person.coins},

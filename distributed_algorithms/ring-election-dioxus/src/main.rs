545119b2d8d3331879b0136bf32802e13c619d70
CT Arrington <arrington.ct@gmail.com>
2022-02-05 12:39:55 -0500

    basic flow

--- distributed_algorithms/ring-election-dioxus/src/main.rs
+++ distributed_algorithms/ring-election-dioxus/src/main.rs
@@ -22,6 +22,7 @@ enum Status {
 #[derive(Debug, Clone)]
 struct Process {
     uid: u32,
+    network_index: usize,
     send_value: Option<Message>,
     input_value: Option<Message>,
     status: Status,
@@ -32,6 +33,7 @@ impl Process {
     fn new() -> Self {
         Process {
             uid: 0,
+            network_index: 0,
             send_value: None,
             input_value: None,
             status: Status::UNKNOWN,
@@ -40,20 +42,54 @@ impl Process {
     }
 
     fn tick(&mut self) {
-        self.send_value = Some(Message::UID(self.uid));
+        match self.input_value {
+            Some(Message::UID(uid)) => {
+                if uid > self.uid {
+                    self.send_value = Some(Message::UID(uid));
+                } else if uid == self.uid {
+                    self.send_value = Some(Message::CORONATION(self.uid));
+                    self.status = Status::LEADER;
+                } else {
+                    self.send_value = None;
+                }
+            }
+
+            Some(Message::CORONATION(uid)) => {
+                if uid > self.uid {
+                    self.status = Status::FOLLOWER(uid);
+                    self.send_value = Some(Message::CORONATION(uid));
+                } else if uid == self.uid {
+                    self.halted = true;
+                    self.send_value = None;
+                } else {
+                    self.send_value = None;
+                }
+            }
+
+            None => {
+                self.send_value = None;
+            }
+        }
     }
 }
 
 #[derive(Clone, Debug)]
-struct Network {
+struct Scenario {
     processes: [Process; PROCESS_COUNT],
     processor_count: usize,
 }
 
-impl Network {
+impl Scenario {
     fn new(processor_count: usize) -> Self {
-        let processes = [0; PROCESS_COUNT].map(|_| Process::new());
-        Network {
+        let mut processes = [0; PROCESS_COUNT].map(|index| Process::new());
+
+        for index in 0..processor_count {
+            processes[index].network_index = index;
+            processes[index].uid = index as u32;
+            processes[index].send_value = Some(Message::UID(index as u32));
+        }
+
+        Scenario {
             processes,
             processor_count,
         }
@@ -62,6 +98,12 @@ impl Network {
     fn tick(&self) -> Self {
         let mut next = self.clone();
         for index in 0..next.processor_count {
+            let source_index = if index == 0 {
+                next.processor_count - 1
+            } else {
+                index - 1
+            };
+            next.processes[index].input_value = self.processes[source_index].send_value;
             next.processes[index].tick();
         }
         next
@@ -75,18 +117,35 @@ fn main() {
 
 #[allow(non_snake_case)]
 fn App(cx: Scope) -> Element {
-    let (network, set_network) = use_state(&cx, || Network::new(3));
-    /*
-    let on_network_tick = move |_| {
-        let next = network.tick();
-        set_network(next);
+    let (scenario, set_scenario) = use_state(&cx, || Scenario::new(3));
+    let on_scenario_tick = move |_| {
+        let next = scenario.tick();
+        set_scenario(next);
     };
-     */
 
-    let processor_count = network.processor_count;
+    let process_list = (0..scenario.processor_count).map(|index| {
+        let process_summary = format!("{:?}", scenario.processes[index]);
+
+        rsx! {
+            div {
+                key: "{index}",
+                "{process_summary}"
+            }
+        }
+    });
+
+    let network_index = scenario.processes[0].network_index;
     cx.render(rsx! {
         div {
-            h1 {"Current Network: {processor_count}"}
+            h1 {"Current Scenarion network_index: {network_index}"}
+            process_list
+            button { onclick: on_scenario_tick, "Scenario+"}
         }
     })
+}
+
+#[test]
+fn test_construction() {
+    let scenario = Scenario::new(3);
+    assert_eq!(scenario.processes[1].uid, 1);
 }

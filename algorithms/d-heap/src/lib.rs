ad4991d6b7d86eb8e6b6effd6a81ee5e53af3d84
ctarrington <arrington.ct@gmail.com>
2022-02-10 22:38:11 -0500

    insert but no top

--- algorithms/d-heap/src/lib.rs
+++ algorithms/d-heap/src/lib.rs
@@ -1,19 +1,39 @@
-const INITIAL_SIZE: usize = 10;
-
 pub struct Heap {
-    values: [Option<i32>; INITIAL_SIZE],
-    end_index: usize,
+    values: Vec<i32>,
+    child_count: usize,
 }
 
 impl Heap {
-    pub fn new() -> Self {
-        const NADA: Option<i32> = None;
-        let values = [NADA; INITIAL_SIZE];
+    pub fn new(child_count: usize) -> Self {
+        let values = vec![];
 
         Heap {
             values,
-            end_index: 0,
+            child_count,
         }
+    }
+
+    fn get_parent_index(&self, index: usize) -> Option<usize> {
+        match index {
+            0 => None,
+            _ => Some((index - 1) / self.child_count),
+        }
+    }
+
+    fn bubble_up(&mut self, index: usize) {
+        let parent_index_option = self.get_parent_index(index);
+
+        if let Some(parent_index) = parent_index_option {
+            if self.values.get(index).unwrap() > self.values.get(parent_index).unwrap() {
+                self.values.swap(index, parent_index);
+                self.bubble_up(parent_index);
+            }
+        }
+    }
+
+    pub fn insert(&mut self, value: i32) {
+        self.values.push(value);
+        self.bubble_up(self.values.len() - 1);
     }
 }
 
@@ -22,9 +42,35 @@ mod tests {
     use crate::Heap;
 
     #[test]
-    fn construction() {
-        let heap = Heap::new();
-        assert_eq!(*heap.values.get(0).unwrap(), None);
-        assert_eq!(heap.end_index, 0);
+    fn insert_binary() {
+        let mut heap = Heap::new(2);
+
+        heap.insert(5);
+        heap.insert(9);
+        heap.insert(3);
+        heap.insert(8);
+
+        //     9
+        //   8   3
+        // 5
+
+        assert_eq!(vec![9, 8, 3, 5], heap.values);
+    }
+
+    #[test]
+    fn insert_ternary() {
+        let mut heap = Heap::new(3);
+
+        heap.insert(5);
+        heap.insert(9);
+        heap.insert(3);
+        heap.insert(8);
+        heap.insert(6);
+
+        //         9
+        //    6    3     8
+        // 5
+
+        assert_eq!(vec![9, 6, 3, 8, 5], heap.values);
     }
 }

2fa997319e89f342ce4a64b5c0a4e368deebbaa2
ctarrington <arrington.ct@gmail.com>
2022-02-13 09:15:37 -0500

    max and min

--- algorithms/d-heap/src/lib.rs
+++ algorithms/d-heap/src/lib.rs
@@ -8,25 +8,33 @@ impl Priority for i32 {
     }
 }
 
+pub enum HeapType {
+    MIN,
+    MAX,
+}
+
 pub struct Heap<T> {
     values: Vec<T>,
     branch_factor: usize,
+    heap_type: HeapType,
 }
 
 impl<T: Priority> Heap<T> {
-    pub fn new(branch_factor: usize) -> Self {
+    pub fn new(branch_factor: usize, heap_type: HeapType) -> Self {
         let values = vec![];
 
         Heap {
             values,
             branch_factor,
+            heap_type,
         }
     }
 
-    pub fn from(branch_factor: usize, initial: Vec<T>) -> Self {
+    pub fn from(branch_factor: usize, heap_type: HeapType, initial: Vec<T>) -> Self {
         let mut heap = Heap {
             values: initial,
             branch_factor,
+            heap_type,
         };
 
         heap.heapify();
@@ -58,13 +66,24 @@ impl<T: Priority> Heap<T> {
         }
     }
 
-    fn get_max_child_index(&self, index: usize) -> Option<usize> {
+    fn compare(&self, candidate_value: i32, comparison_value: i32) -> bool {
+        match self.heap_type {
+            HeapType::MAX => candidate_value > comparison_value,
+            HeapType::MIN => candidate_value < comparison_value,
+        }
+    }
+
+    fn get_extreme_child_index(&self, index: usize) -> Option<usize> {
         if self.values.is_empty() || index * self.branch_factor >= self.last_index() {
             return None;
         }
 
-        let mut max_child_value = 0;
-        let mut max_child_index = 0;
+        let mut extreme_child_value = match self.heap_type {
+            HeapType::MIN => i32::MAX,
+            HeapType::MAX => i32::MIN,
+        };
+
+        let mut extreme_child_index = 0;
         for offset in 1..=self.branch_factor {
             let candidate_index = index * self.branch_factor + offset;
             if candidate_index > self.last_index() {
@@ -72,20 +91,21 @@ impl<T: Priority> Heap<T> {
             }
 
             let candidate_value = self.values.get(candidate_index).unwrap().get_priority();
-            if candidate_value > max_child_value {
-                max_child_index = candidate_index;
-                max_child_value = candidate_value;
+            if self.compare(candidate_value, extreme_child_value) {
+                extreme_child_index = candidate_index;
+                extreme_child_value = candidate_value;
             }
         }
 
-        Some(max_child_index)
+        Some(extreme_child_index)
     }
 
     fn bubble_up(&mut self, index: usize) {
         if let Some(parent_index) = self.get_parent_index(index) {
-            if self.values.get(index).unwrap().get_priority()
-                > self.values.get(parent_index).unwrap().get_priority()
-            {
+            if self.compare(
+                self.values.get(index).unwrap().get_priority(),
+                self.values.get(parent_index).unwrap().get_priority(),
+            ) {
                 self.values.swap(index, parent_index);
                 self.bubble_up(parent_index);
             }
@@ -99,17 +119,19 @@ impl<T: Priority> Heap<T> {
 
         let last_parent_index = self.get_parent_index(self.last_index()).unwrap();
         for index in (0..=last_parent_index).rev() {
+            println!("before push down {}", index);
             self.push_down(index);
         }
     }
 
     fn push_down(&mut self, index: usize) {
-        if let Some(max_child_index) = self.get_max_child_index(index) {
-            if self.values.get(max_child_index).unwrap().get_priority()
-                > self.values.get(index).unwrap().get_priority()
-            {
-                self.values.swap(index, max_child_index);
-                self.push_down(max_child_index);
+        if let Some(extreme_child_index) = self.get_extreme_child_index(index) {
+            if self.compare(
+                self.values.get(extreme_child_index).unwrap().get_priority(),
+                self.values.get(index).unwrap().get_priority(),
+            ) {
+                self.values.swap(index, extreme_child_index);
+                self.push_down(extreme_child_index);
             }
         }
     }
@@ -143,11 +165,11 @@ impl<T: Priority> Iterator for HeapIntoIter<T> {
 
 #[cfg(test)]
 mod tests {
-    use crate::{Heap, Priority};
+    use crate::{Heap, HeapType, Priority};
 
     #[test]
     fn insert_binary() {
-        let mut heap = Heap::new(2);
+        let mut heap = Heap::new(2, HeapType::MAX);
 
         heap.insert(5);
         heap.insert(9);
@@ -162,8 +184,24 @@ mod tests {
     }
 
     #[test]
+    fn insert_binary_min() {
+        let mut heap = Heap::new(2, HeapType::MIN);
+
+        heap.insert(5);
+        heap.insert(9);
+        heap.insert(3);
+        heap.insert(8);
+
+        //     3
+        //   8   5
+        // 9
+
+        assert_eq!(vec![3, 8, 5, 9], heap.values);
+    }
+
+    #[test]
     fn all_ones_binary() {
-        let mut heap = Heap::new(2);
+        let mut heap = Heap::new(2, HeapType::MAX);
 
         heap.insert(1);
         heap.insert(1);
@@ -174,7 +212,7 @@ mod tests {
 
     #[test]
     fn insert_ternary() {
-        let mut heap = Heap::new(3);
+        let mut heap = Heap::new(3, HeapType::MAX);
 
         heap.insert(5);
         heap.insert(9);
@@ -191,7 +229,7 @@ mod tests {
 
     #[test]
     fn from_binary() {
-        let mut heap = Heap::from(2, vec![5, 9, 10, 8]);
+        let mut heap = Heap::from(2, HeapType::MAX, vec![5, 9, 10, 8]);
 
         //         5                         10
         //    9        10   =>          9          5
@@ -204,8 +242,27 @@ mod tests {
     }
 
     #[test]
+    fn from_binary_small_max() {
+        let mut heap = Heap::from(2, HeapType::MAX, vec![20, 10, 30]);
+
+        assert_eq!(vec![30, 10, 20], heap.values);
+        assert_eq!(Some(30), heap.top());
+        assert_eq!(Some(20), heap.top());
+        assert_eq!(Some(10), heap.top());
+    }
+    #[test]
+    fn from_binary_small_min() {
+        let mut heap = Heap::from(2, HeapType::MIN, vec![20, 30, 10]);
+
+        assert_eq!(vec![10, 30, 20], heap.values);
+        assert_eq!(Some(10), heap.top());
+        assert_eq!(Some(20), heap.top());
+        assert_eq!(Some(30), heap.top());
+    }
+
+    #[test]
     fn top_binary() {
-        let mut heap = Heap::new(2);
+        let mut heap = Heap::new(2, HeapType::MAX);
 
         heap.insert(5);
         heap.insert(9);
@@ -231,6 +288,7 @@ mod tests {
 
     #[test]
     fn priority_messages_with_iteration() {
+        #[derive(Clone)]
         struct Message {
             text: String,
             priority: i32,
@@ -257,8 +315,12 @@ mod tests {
             },
         ];
 
-        let heap = Heap::from(2, messages);
+        let heap = Heap::from(2, HeapType::MAX, messages.clone());
         let ordered_text: Vec<String> = heap.into_iter().map(|message| message.text).collect();
         assert_eq!(vec!["Hi", "Ho", "Silver"], ordered_text);
+
+        let heap = Heap::from(2, HeapType::MIN, messages.clone());
+        let ordered_text: Vec<String> = heap.into_iter().map(|message| message.text).collect();
+        assert_eq!(vec!["Silver", "Ho", "Hi"], ordered_text);
     }
 }

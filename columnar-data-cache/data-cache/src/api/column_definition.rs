dd9f8f2b6519ac47f56704cf573a54e059c2f0da
CT Arrington <arrington.ct@gmail.com>
2023-11-05 08:55:12 -0500

    clean up and move units to numeric column type

--- columnar-data-cache/data-cache/src/api/column_definition.rs
+++ columnar-data-cache/data-cache/src/api/column_definition.rs
@@ -1,10 +1,22 @@
 use chrono::NaiveDateTime;
 
-/// Column definition
+/// The structs in this file are used to define the columns for a data cache.
+/// They allow client code to define the type of each column.
+/// Also, the default value is used when the client code does not provide a value when parsing
+/// a row of data. The details of the parsing are handled by the column types
+///
+/// There are a few subtleties to note:
+/// 1. The column type is an enum, but the parse method is implemented on each of the enum variants.
+/// 2. The DateTimeColumnType struct has a format string that is used to parse the date.
+///    The passed data value must match the format string or a panic will occur.
 
 // This is a helper function that returns the value of a column based on the passed value and the
 // default value for the column definition.
-fn get_value(column_definition: ColumnDefinition, value: &str) -> Option<String> {
+
+// todo: units should be an enum
+// todo: implement precision for NumericColumnType
+
+fn get_value(column_definition: &ColumnDefinition, value: &str) -> Option<String> {
     if value.len() > 0 {
         Some(value.to_string())
     } else if column_definition.get_default_value().len() > 0 {
@@ -18,7 +30,7 @@ fn get_value(column_definition: ColumnDefinition, value: &str) -> Option<String>
 pub struct StringColumnType {}
 
 impl StringColumnType {
-    pub fn parse(&self, column_definition: ColumnDefinition, value: &str) -> Option<String> {
+    pub fn parse(&self, column_definition: &ColumnDefinition, value: &str) -> Option<String> {
         get_value(column_definition, value)
     }
 }
@@ -30,9 +42,9 @@ pub struct BooleanColumnType {}
 /// and "false" and "0" as false.
 /// Blank is not false, but rather None.
 impl BooleanColumnType {
-    pub fn parse(&self, column_definition: ColumnDefinition, value: &str) -> Option<bool> {
+    pub fn parse(&self, column_definition: &ColumnDefinition, value: &str) -> Option<bool> {
         match get_value(column_definition, value) {
-            Some(the_value) => match the_value.as_str() {
+            Some(the_value) => match the_value.as_str().to_ascii_lowercase().trim() {
                 "true" => Some(true),
                 "false" => Some(false),
                 "0" => Some(false),
@@ -53,9 +65,9 @@ pub struct DateTimeColumnType {
 impl DateTimeColumnType {
     pub fn parse(
         &self,
-        column_definition: ColumnDefinition,
+        column_definition: &ColumnDefinition,
         value: &str,
-    ) -> Option<chrono::NaiveDateTime> {
+    ) -> Option<NaiveDateTime> {
         let the_value = get_value(column_definition, value);
         match the_value {
             Some(the_value) => Some(
@@ -68,10 +80,11 @@ impl DateTimeColumnType {
 #[derive(Debug, PartialEq)]
 pub struct NumericColumnType {
     precision: u8,
+    units: String,
 }
 
 impl NumericColumnType {
-    pub fn parse(&self, column_definition: ColumnDefinition, value: &str) -> Option<f64> {
+    pub fn parse(&self, column_definition: &ColumnDefinition, value: &str) -> Option<f64> {
         let the_value = get_value(column_definition, value);
         match the_value {
             Some(the_value) => Some(the_value.parse::<f64>().unwrap()),
@@ -91,17 +104,15 @@ pub enum ColumnType {
 pub struct ColumnDefinition {
     name: String,
     view_name: String,
-    units: String,
     column_type: ColumnType,
     default_value: String,
 }
 
 impl ColumnDefinition {
-    pub fn new(name: String, view_name: String, units: String, default_value: String) -> Self {
+    pub fn new(name: String, view_name: String, default_value: String) -> Self {
         Self {
             name,
             view_name,
-            units,
             column_type: ColumnType::StringColumnType(StringColumnType {}),
             default_value,
         }
@@ -113,10 +124,6 @@ impl ColumnDefinition {
 
     pub fn get_view_name(&self) -> &str {
         &self.view_name
-    }
-
-    pub fn get_units(&self) -> &str {
-        &self.units
     }
 
     pub fn get_column_type(&self) -> &ColumnType {
@@ -132,11 +139,12 @@ impl ColumnDefinition {
 mod tests {
     use super::*;
 
+    const DATE_TIME_FORMAT: &str = "%Y-%m-%d %H:%M:%S";
+
     fn generate_string_column_definition() -> ColumnDefinition {
         ColumnDefinition {
             name: "name".parse().unwrap(),
             view_name: "The Name".to_string(),
-            units: "".to_string(),
             column_type: ColumnType::StringColumnType(StringColumnType {}),
             default_value: "-".to_string(),
         }
@@ -144,9 +152,8 @@ mod tests {
 
     fn generate_boolean_column_definition() -> ColumnDefinition {
         ColumnDefinition {
-            name: "ok".parse().unwrap(),
+            name: "ok".to_string(),
             view_name: "OK".to_string(),
-            units: "".to_string(),
             column_type: ColumnType::BooleanColumnType(BooleanColumnType {}),
             default_value: "".to_string(),
         }
@@ -155,20 +162,21 @@ mod tests {
     fn generate_numeric_column_definition() -> ColumnDefinition {
         ColumnDefinition {
             name: "speed".parse().unwrap(),
-            view_name: "Speedd".to_string(),
-            units: "meters/second".to_string(),
-            column_type: ColumnType::NumericColumnType(NumericColumnType { precision: 2 }),
+            view_name: "Speed".to_string(),
+            column_type: ColumnType::NumericColumnType(NumericColumnType {
+                precision: 2,
+                units: "meters/second".to_string(),
+            }),
             default_value: "".to_string(),
         }
     }
 
     fn generate_datetime_column_definition() -> ColumnDefinition {
         ColumnDefinition {
-            name: "startdate".parse().unwrap(),
+            name: "startDate".parse().unwrap(),
             view_name: "Start Date".to_string(),
-            units: "".to_string(),
             column_type: ColumnType::DateTimeColumnType(DateTimeColumnType {
-                format: "%Y-%m-%d %H:%M:%S".to_string(),
+                format: DATE_TIME_FORMAT.to_string(),
             }),
             default_value: "".to_string(),
         }
@@ -177,41 +185,46 @@ mod tests {
     #[test]
     fn test_string_column_type() {
         let string_column_type = StringColumnType {};
+        let definition = generate_string_column_definition();
         assert_eq!(
-            string_column_type.parse(generate_string_column_definition(), "value"),
+            string_column_type.parse(&definition, "value"),
             Some("value".to_string())
+        );
+        assert_eq!(
+            string_column_type.parse(&definition, ""),
+            Some("-".to_string())
         );
     }
 
     #[test]
     fn test_boolean_column_type() {
         let boolean_column_type = BooleanColumnType {};
-        assert_eq!(
-            boolean_column_type.parse(generate_boolean_column_definition(), "true"),
-            Some(true)
-        );
+        let definition = generate_boolean_column_definition();
+        assert_eq!(boolean_column_type.parse(&definition, "true"), Some(true));
+        assert_eq!(boolean_column_type.parse(&definition, "True"), Some(true));
     }
 
     #[test]
     fn test_numeric_column_type() {
-        let numeric_column_type = NumericColumnType { precision: 2 };
-        assert_eq!(
-            numeric_column_type.parse(generate_numeric_column_definition(), "1.0"),
-            Some(1.0)
-        );
+        let numeric_column_type = NumericColumnType {
+            precision: 2,
+            units: "meters/second".to_string(),
+        };
+        let definition = generate_numeric_column_definition();
+        assert_eq!(numeric_column_type.parse(&definition, "1.0"), Some(1.0));
+        assert_eq!(numeric_column_type.parse(&definition, ""), None);
+        assert_eq!(numeric_column_type.units, "meters/second".to_string());
     }
 
     #[test]
     fn test_datetime_column_type() {
         let datetime_column_type = DateTimeColumnType {
-            format: "%Y-%m-%d %H:%M:%S".to_string(),
+            format: DATE_TIME_FORMAT.to_string(),
         };
+        let definition = generate_datetime_column_definition();
         assert_eq!(
-            datetime_column_type
-                .parse(generate_datetime_column_definition(), "2020-01-01 00:00:00"),
-            Some(
-                NaiveDateTime::parse_from_str("2020-01-01 00:00:00", "%Y-%m-%d %H:%M:%S").unwrap()
-            )
+            datetime_column_type.parse(&definition, "2020-01-01 00:00:00"),
+            Some(NaiveDateTime::parse_from_str("2020-01-01 00:00:00", DATE_TIME_FORMAT).unwrap())
         );
     }
 
@@ -220,11 +233,22 @@ mod tests {
         let column_definition = generate_string_column_definition();
         assert_eq!(column_definition.get_name(), "name");
         assert_eq!(column_definition.get_view_name(), "The Name");
-        assert_eq!(column_definition.get_units(), "");
         assert_eq!(
             column_definition.get_column_type(),
             &ColumnType::StringColumnType(StringColumnType {})
         );
         assert_eq!(column_definition.get_default_value(), "-");
+    }
+
+    #[test]
+    fn test_boolean_column_definition() {
+        let column_definition = generate_boolean_column_definition();
+        assert_eq!(column_definition.get_name(), "ok");
+        assert_eq!(column_definition.get_view_name(), "OK");
+        assert_eq!(
+            column_definition.get_column_type(),
+            &ColumnType::BooleanColumnType(BooleanColumnType {})
+        );
+        assert_eq!(column_definition.get_default_value(), "");
     }
 }

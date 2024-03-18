0b18e0f25709bc7e457aa301da9b0363eac83964
CT Arrington <arrington.ct@gmail.com>
2024-03-18 16:47:01 -0400

    more patterns for error checking futures

--- rust-web-development-gruber/ch02-async-await/src/main.rs
+++ rust-web-development-gruber/ch02-async-await/src/main.rs
@@ -3,6 +3,7 @@ use std::io::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Option 1 if let pattern
     let response = reqwest::get("https://httpbin.org/ip").await;
 
     if let Err(e) = response {
@@ -23,6 +24,30 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let json = json?;
     println!("origin: {}", json["origin"]);
+
+    // Option 2 match pattern
+    match reqwest::get("https://httpbin.org/ip").await {
+        Ok(response) => match response.json::<HashMap<String, String>>().await {
+            Ok(json) => {
+                println!("origin: {}", json["origin"]);
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                return Err(Error::new(std::io::ErrorKind::Other, "Error on parse").into());
+            }
+        },
+        Err(e) => {
+            println!("Error: {}", e);
+            return Err(Error::new(std::io::ErrorKind::Other, "Error on request").into());
+        }
+    };
+
+    // Option 3: fafo panic pattern
+    let response_map = reqwest::get("https://httpbin.org/ip")
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+    println!("origin: {}", response_map["origin"]);
 
     Ok(())
 }

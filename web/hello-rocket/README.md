d810059f1208c7b26e0e3d18eaf097549f91b62b
CT Arrington <arrington.ct@gmail.com>
2024-06-21 23:46:45 -0400

    server sent events

--- web/hello-rocket/README.md
+++ web/hello-rocket/README.md
@@ -1,4 +1,13 @@
-## run
+### run
+
 cargo watch -x "run --release"
 
+### listen for server sent events
+
+curl -N http://localhost:8000/events
+
+### send a message to the server
+
+curl -X POST http://localhost:8000/message -H "Content-Type: application/x-www-form-urlencoded" \
+-d "from=fred&content=hello"
 
--- web/hello-rocket/src/main.rs
+++ web/hello-rocket/src/main.rs
@@ -1,12 +1,54 @@
 #[macro_use]
 extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+use rocket::form::Form;
+use rocket::fs::{relative, FileServer};
+use rocket::response::stream::{Event, EventStream};
+use rocket::serde::{Deserialize, Serialize};
+use rocket::tokio::select;
+use rocket::tokio::sync::broadcast::{channel, error::RecvError, Sender};
+use rocket::{Shutdown, State};
+
+#[derive(Debug, Clone, FromForm, Serialize, Deserialize)]
+#[cfg_attr(test, derive(PartialEq, UriDisplayQuery))]
+#[serde(crate = "rocket::serde")]
+struct Message {
+    from: String,
+    content: String,
+}
+
+/// Returns an infinite stream of server-sent events. Each event is a message
+/// pulled from a broadcast queue sent by the `post` handler.
+#[get("/events")]
+async fn events(queue: &State<Sender<Message>>, mut end: Shutdown) -> EventStream![] {
+    let mut rx = queue.subscribe();
+    EventStream! {
+        loop {
+            let msg = select! {
+                msg = rx.recv() => match msg {
+                    Ok(msg) => msg,
+                    Err(RecvError::Closed) => break,
+                    Err(RecvError::Lagged(_)) => continue,
+                },
+                _ = &mut end => break,
+            };
+
+            yield Event::json(&msg);
+        }
+    }
+}
+
+/// Receive a message from a form submission and broadcast it to any receivers.
+#[post("/message", data = "<form>")]
+fn post(form: Form<Message>, queue: &State<Sender<Message>>) {
+    // A send 'fails' if there are no active subscribers. That's okay.
+    let _res = queue.send(form.into_inner());
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .manage(channel::<Message>(1024).0)
+        .mount("/", routes![post, events])
+        .mount("/public", FileServer::from(relative!("public")))
 }

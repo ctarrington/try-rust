401ad6cb67c5123dbac1b00657c155faae08db04
CT Arrington <arrington.ct@gmail.com>
2024-12-22 14:26:16 -0500

    clean up

--- option-and-result/src/main.rs
+++ option-and-result/src/main.rs
@@ -2,37 +2,77 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn capitalize(words: Vec<Option<String>>) -> Vec<String> {
+fn capitalize(word: &str) -> Option<String> {
+    Some(word.to_uppercase())
+}
+
+fn process(word: &str) -> Option<&str> {
+    match word {
+        "Skip" => None,
+        word => Some(word),
+    }
+}
+
+// flatten drops out any Nones
+fn capitalize_all(words: Vec<Option<&str>>) -> Vec<String> {
     words
         .iter()
-        .map(|w| w.clone().and_then(|w| Some(w.to_uppercase())))
+        .map(|w| w.and_then(process).and_then(capitalize))
         .flatten()
         .collect()
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::capitalize;
+    use crate::{capitalize, capitalize_all, process};
+
+    #[test]
+    fn skips() {
+        let word = Some("Skip");
+        assert_eq!(word.and_then(process).and_then(capitalize), None);
+    }
+
+    #[test]
+    fn caps() {
+        let word = Some("Hi");
+        assert_eq!(
+            word.and_then(process).and_then(capitalize),
+            Some("HI".to_string())
+        );
+    }
 
     #[test]
     fn all_there() {
-        let words = vec![Some("hi".to_string()), Some("there".to_string())];
-        let result = capitalize(words);
-        assert_eq!(result, vec!["HI".to_string(), "THERE".to_string()]);
+        let words = vec![Some("hi"), Some("there")];
+        let result = capitalize_all(words);
+        assert_eq!(result, vec!["HI", "THERE"]);
     }
 
     #[test]
     fn some_there() {
-        let words = vec![Some("hi".to_string()), None, Some("there".to_string())];
-        let result = capitalize(words);
-        assert_eq!(result, vec!["HI".to_string(), "THERE".to_string()]);
+        let words = vec![Some("hi"), None, Some("there")];
+        let result = capitalize_all(words);
+        assert_eq!(result, vec!["HI", "THERE"]);
+    }
+
+    #[test]
+    fn skip_there() {
+        let words = vec![Some("hi"), Some("Skip"), Some("there")];
+        let result = capitalize_all(words);
+        assert_eq!(result, vec!["HI", "THERE"]);
     }
 
     #[test]
     fn none_there() {
         let empty: Vec<String> = vec![];
-        let words = vec![None, None];
-        let result = capitalize(words);
+        let words = vec![None, Some("Skip")];
+        let result = capitalize_all(words);
         assert_eq!(result, empty);
+    }
+
+    #[test]
+    fn test_process() {
+        assert_eq!(process("Skip"), None);
+        assert_eq!(process("hi"), Some("hi"));
     }
 }

e242cc3e734384dd16a9563c4109fe9e802164f1
CT Arrington <arrington.ct@gmail.com>
2024-12-29 14:46:18 -0500

    mutex to protect file

--- design-patterns/src/singleton_lazy_lock.rs
+++ design-patterns/src/singleton_lazy_lock.rs
@@ -1,12 +1,13 @@
 use std::fs;
 use std::string::ToString;
-use std::sync::LazyLock;
+use std::sync::{LazyLock, Mutex};
 
 #[derive(Default)]
+// only accessible through the AccessCount to prevent races
 struct AccessCountFile {}
 
 impl AccessCountFile {
-    fn get_access_count(&self) -> usize {
+    fn get_count(&self) -> usize {
         let filename = "./.access_count.txt";
         let file_contents = fs::read_to_string(filename);
         match file_contents {
@@ -26,19 +27,35 @@ impl AccessCountFile {
         }
     }
 
-    fn increment_access_count(&self) {
+    fn increment_count(&self) {
         let filename = "./.access_count.txt";
-        let value = self.get_access_count();
+        let value = self.get_count();
         let new_value = value + 1;
 
         fs::write(filename, format!("{new_value}")).expect("failed to write to file");
     }
 }
 
-static ACCESS_COUNT_FILE: LazyLock<AccessCountFile> = LazyLock::new(|| AccessCountFile::default());
+#[derive(Default)]
+/// Thread safe access count keeper
+pub struct AccessCount {
+    inner: Mutex<AccessCountFile>,
+}
+
+impl AccessCount {
+    pub fn get_access_count(&self) -> usize {
+        self.inner.lock().unwrap().get_count()
+    }
+
+    pub fn increment_access_count(&self) {
+        self.inner.lock().unwrap().increment_count()
+    }
+}
+
+static ACCESS_COUNT: LazyLock<AccessCount> = LazyLock::new(AccessCount::default);
 
 static CONFIG: LazyLock<String> = LazyLock::new(|| {
-    ACCESS_COUNT_FILE.increment_access_count();
+    ACCESS_COUNT.increment_access_count();
     println!("loading config");
 
     "This is the best config".to_string()
@@ -50,22 +67,22 @@ pub fn get_config() -> &'static String {
 
 #[cfg(test)]
 mod tests {
-    use crate::singleton_lazy_lock::{get_config, AccessCountFile, ACCESS_COUNT_FILE};
+    use crate::singleton_lazy_lock::{get_config, ACCESS_COUNT};
     use std::thread;
     use std::time::Duration;
 
     #[test]
     fn test_get_config() {
-        let original_access_count = ACCESS_COUNT_FILE.get_access_count();
+        let original_access_count = ACCESS_COUNT.get_access_count();
         assert_eq!(get_config(), &"This is the best config".to_string());
         assert_eq!(get_config(), &"This is the best config".to_string());
-        let final_access_count = ACCESS_COUNT_FILE.get_access_count();
+        let final_access_count = ACCESS_COUNT.get_access_count();
         assert_eq!(final_access_count, original_access_count + 1);
     }
 
     #[test]
     fn test_get_config_threaded() {
-        let original_access_count = ACCESS_COUNT_FILE.get_access_count();
+        let original_access_count = ACCESS_COUNT.get_access_count();
         let mut handles = vec![];
 
         for ctr in 1..5 {
@@ -84,7 +101,7 @@ mod tests {
         }
         println!("all done");
 
-        let final_access_count = ACCESS_COUNT_FILE.get_access_count();
+        let final_access_count = ACCESS_COUNT.get_access_count();
         assert_eq!(final_access_count, original_access_count + 1);
         println!("final_access_count {final_access_count}")
     }

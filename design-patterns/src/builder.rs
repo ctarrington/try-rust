0e12c4a909377ae8f2d858ab01e7338c4f3096c5
CT Arrington <arrington.ct@gmail.com>
2024-12-30 17:47:40 -0500

    fluent builder

--- design-patterns/src/builder.rs
+++ design-patterns/src/builder.rs
@@ -20,12 +20,14 @@ pub struct WidgetBuilder {
 }
 
 impl WidgetBuilder {
-    pub fn with_size(&mut self, size: u32) {
+    pub fn with_size(mut self, size: u32) -> Self {
         self.widget.size = size;
+        self
     }
 
-    pub fn with_color(&mut self, color: &str) {
+    pub fn with_color(mut self, color: &str) -> Self {
         self.widget.color = color.parse().unwrap();
+        self
     }
 
     pub fn make(self) -> Widget {
@@ -38,12 +40,23 @@ mod tests {
     use crate::builder::{Widget, WidgetBuilder};
 
     #[test]
-    fn create_widget() {
-        let mut builder = WidgetBuilder::default();
-        builder.with_size(10);
-        builder.with_color("blue");
-        let widget: Widget = builder.make();
+    fn fluent_widget_creation() {
+        let widget: Widget = WidgetBuilder::default()
+            .with_size(10)
+            .with_color("blue")
+            .make();
         assert_eq!(widget.color(), "blue");
         assert_eq!(widget.size(), 10);
+    }
+
+    #[test]
+    fn non_fluent_widget_creation() {
+        let builder = WidgetBuilder::default();
+        // need to receive the moved builder
+        let builder = builder.with_size(11);
+        let builder = builder.with_color("red");
+        let widget: Widget = builder.make();
+        assert_eq!(widget.color(), "red");
+        assert_eq!(widget.size(), 11);
     }
 }

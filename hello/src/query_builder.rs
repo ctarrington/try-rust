f9b580d42394d84a56e3751d177738fdef110830
CT Arrington <arrington.ct@gmail.com>
2025-01-11 21:51:03 -0500

    clean up

--- hello/src/query_builder.rs
+++ hello/src/query_builder.rs
@@ -1,27 +1,43 @@
 #[cfg(test)]
 mod tests {
-    fn build_fragment(field: &str, operator: i8, value: Option<f32>) -> String {
+    use crate::query_builder::tests::Operator::{Greater, Less};
+
+    #[derive(Debug)]
+    enum Operator {
+        Greater,
+        Less,
+    }
+
+    impl Operator {
+        fn flip(self) -> Operator {
+            match self {
+                Greater => Less,
+                Less => Greater,
+            }
+        }
+    }
+
+    fn build_fragment(field: &str, operator: Operator, value: Option<f32>) -> String {
         match value {
             None => "".to_string(),
             Some(value) => {
-                let op = if value > 0 as f32 {
+                let operator = if value > 0 as f32 {
                     operator
                 } else {
-                    -operator
+                    operator.flip()
                 };
-                let word = if op > 0 { "greater" } else { "less" };
-
-                format!("&filter={word}:{field}:{value}")
+                let operator = format!("{operator:?}").to_lowercase();
+                format!("&filter={operator}:{field}:{value}")
             }
         }
     }
 
     fn build_geo_query(criteria: &TrackCriteria) -> String {
         [
-            build_fragment("latitude", 1, criteria.min_latitude_radians),
-            build_fragment("latitude", -1, criteria.max_latitude_radians),
-            build_fragment("longitude", 1, criteria.min_longitude_radians),
-            build_fragment("longitude", -1, criteria.max_longitude_radians),
+            build_fragment("latitude", Greater, criteria.min_latitude_radians),
+            build_fragment("latitude", Less, criteria.max_latitude_radians),
+            build_fragment("longitude", Greater, criteria.min_longitude_radians),
+            build_fragment("longitude", Less, criteria.max_longitude_radians),
         ]
         .join("")
     }
@@ -35,25 +51,25 @@ mod tests {
 
     #[test]
     fn test_greater_than_positive() {
-        let fragment = build_fragment("latitude", 1, Some(0.11));
+        let fragment = build_fragment("latitude", Greater, Some(0.11));
         assert_eq!(fragment, "&filter=greater:latitude:0.11");
     }
 
     #[test]
     fn test_greater_than_negative() {
-        let fragment = build_fragment("latitude", 1, Some(-0.11));
+        let fragment = build_fragment("latitude", Greater, Some(-0.11));
         assert_eq!(fragment, "&filter=less:latitude:-0.11");
     }
 
     #[test]
     fn test_less_than_positive() {
-        let fragment = build_fragment("latitude", -1, Some(0.11));
+        let fragment = build_fragment("latitude", Less, Some(0.11));
         assert_eq!(fragment, "&filter=less:latitude:0.11");
     }
 
     #[test]
     fn test_less_than_negative() {
-        let fragment = build_fragment("latitude", -1, Some(-0.11));
+        let fragment = build_fragment("latitude", Less, Some(-0.11));
         assert_eq!(fragment, "&filter=greater:latitude:-0.11");
     }
 

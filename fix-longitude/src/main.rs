0b22fd1eb518e92aef325da66263ae02005ad2da
CT Arrington <arrington.ct@gmail.com>
2025-05-01 13:37:44 -0400

    invert range check as needed

--- fix-longitude/src/main.rs
+++ fix-longitude/src/main.rs
@@ -1,24 +1,7 @@
 fn main() {
     println!("Hello, world!");
-    let range = convert_to_ranges(50.0, 60.0);
-    println!("{:?}", range);
     let in_bounds = is_in_bounds(50.0, 60.0, 55.0);
     println!("{:?}", in_bounds);
-}
-
-type Ranges = [Option<(f64, f64)>; 2];
-
-fn convert_to_ranges(left_longitude: f64, right_longitude: f64) -> Ranges {
-    if left_longitude < right_longitude {
-        [Some((left_longitude, right_longitude)), None::<(f64, f64)>]
-    } else if left_longitude >= 0.0 && right_longitude < 0.0 {
-        [
-            Some((left_longitude, 180.0)),
-            Some((-180.0, right_longitude)),
-        ]
-    } else {
-        [Some((left_longitude, 180.0)), Some((0.0, right_longitude))]
-    }
 }
 
 fn is_in_bounds(left_longitude: f64, right_longitude: f64, value: f64) -> bool {
@@ -34,21 +17,7 @@ fn is_in_bounds(left_longitude: f64, right_longitude: f64, value: f64) -> bool {
 
 #[cfg(test)]
 mod tests {
-    use crate::{Ranges, convert_to_ranges, is_in_bounds};
-
-    #[test]
-    fn normal_values() {
-        let [primary, secondary]: Ranges = convert_to_ranges(50.0, 60.0);
-        assert_eq!(primary, Some((50.0, 60.0)));
-        assert_eq!(secondary, None::<(f64, f64)>);
-    }
-
-    #[test]
-    fn straddle_meridian() {
-        let [primary, secondary]: Ranges = convert_to_ranges(170.0, 10.0);
-        assert_eq!(primary, Some((170.0, 180.0)));
-        assert_eq!(secondary, Some((0.0, 10.0)));
-    }
+    use crate::is_in_bounds;
 
     #[test]
     fn normal_in_bounds() {

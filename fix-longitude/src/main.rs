f352704c117fc6553dbf808fab654e8df988f81e
CT Arrington <arrington.ct@gmail.com>
2025-05-01 09:35:12 -0400

    range check for longitude around anti meridian

--- fix-longitude/src/main.rs
+++ fix-longitude/src/main.rs
@@ -63,7 +63,7 @@ mod tests {
         assert!(is_in_bounds(170.0, 10.0, 5.0));
         assert!(is_in_bounds(-100.0, -80.0, -90.0));
         assert!(is_in_bounds(-10.0, 10.0, 1.0));
-        assert!(is_in_bounds(151.0, -177.0, 177.0));
+        assert!(is_in_bounds(151.0, -177.0, 179.9));
         assert!(is_in_bounds(151.0, -177.0, -178.0));
     }
 
@@ -74,7 +74,7 @@ mod tests {
         assert!(!is_in_bounds(-100.0, -80.0, -110.0));
         assert!(!is_in_bounds(-100.0, -80.0, -70.0));
         assert!(!is_in_bounds(-10.0, 10.0, 11.0));
-        assert!(!is_in_bounds(151.0, -177.0, -167.0));
-        assert!(!is_in_bounds(151.0, -177.0, 148.0));
+        assert!(!is_in_bounds(151.0, -177.0, -176.0));
+        assert!(!is_in_bounds(151.0, -177.0, 150.9));
     }
 }
